5464ea6f18ab73a58c905cff7e5799ee7cb25355
Nichaen <42658761+Nichaen@users.noreply.github.com>
2020-01-05 02:40:29 +0100

    make better xd
    Discord: Nichaen#0288

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@ using dnlib.DotNet.Writer;
 using LoGiC.NET.Protections;
 using SharpConfigParser;
 using LoGiC.NET.Utils;
+using System.Reflection;
+
 
 namespace LoGiC.NET
 {
@@ -16,19 +18,25 @@ namespace LoGiC.NET
 
         public static bool DontRename { get; set; }
         public static bool ForceWinForms { get; set; }
+        static string spoof;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Drag & drop your file : ");
-            string path = Console.ReadLine();
+            //Console.WriteLine("Drag & drop your file : ");
+            //string path = Console.ReadLine();
 
             Console.WriteLine("Preparing obfuscation...");
             Parser p = new Parser() { ConfigFile = "config.txt" };
             ForceWinForms = bool.Parse(p.Read("ForceWinFormsCompatibility").ReadResponse().ReplaceSpaces());
             DontRename = bool.Parse(p.Read("DontRename").ReadResponse().ReplaceSpaces());
 
-            Module = ModuleDefMD.Load(path);
-            FileExtension = Path.GetExtension(path);
+            foreach (var hi in args)
+            {
+                spoof = hi;
+            }
+            
+            Module = ModuleDefMD.Load(spoof);
+            FileExtension = Path.GetExtension(spoof);
 
             Console.WriteLine("Renaming...");
             Renamer.Execute();
@@ -50,8 +58,16 @@ namespace LoGiC.NET
 
             Console.WriteLine("Saving file...");
             ModuleWriterOptions opts = new ModuleWriterOptions(Module) { Logger = DummyLogger.NoThrowInstance };
-            Module.Write(@"C:\Users\" + Environment.UserName + @"\Desktop\" + Path.GetFileNameWithoutExtension(path) + "_protected" +
-                FileExtension, opts);
+            
+            var loadasm = Assembly.LoadFrom(spoof);
+
+            var asmanme = loadasm.ManifestModule.Name;
+
+            var path = spoof.Replace(asmanme, @"\Obfuscated\") + asmanme;
+
+            Directory.CreateDirectory(spoof.Replace(asmanme, @"\Obfuscated"));//Create
+
+            Module.Write(path, opts);
 
             Console.WriteLine("Done! Press any key to exit...");
             Console.ReadKey();
